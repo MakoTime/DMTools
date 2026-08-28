1eb348844fcad18ed70cbf6647d445528668b157
MakoTime <ben.vernon.bosley@gmail.com>
2026-08-28 23:35:50 +1200

    Race testing under schema

--- tests/schema_importer.py
+++ tests/schema_importer.py
@@ -6,6 +6,7 @@ class SchemaImporter:
         "creatures": "entities/Creature.schema.json",
         "items": "entities/Item.schema.json",
         "spells": "entities/Spell.schema.json",
+        "races": "entities/Race.schema.json",
     }
 
     def __init__(self, schema_root: Path, data_root: Path):
